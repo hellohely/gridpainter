3db1edf1fd444ba6f4ca0dad953b7afea9e64f6d
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-09-05 20:16:09 +0200

    Building basics

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+const chatForm = document.getElementById('chat-form');
+
+const socket = io();
+
+socket.on('message', message => {
+    console.log(message);
+})
+
+// Message submit
+chatForm.addEventListener('sumbit', e => {
+    e.preventDefault();
+    const msg = e.target.elements.msg.value;
+    console.log(msg);
+})
--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const path = require('path')
-
+const path = require('path');
+const http = require('http')
 const express = require('express');
-const app = express();
+const socketio = require('socket.io');
 
-app.use(express.static(path.join(__dirname, 'public')))
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when user connects
+
+io.on('connection', socket => {
+    socket.emit('message', `Välkommen till Gridpainter`)
+
+    // Broadcast when user connects
+    socket.broadcast.emit('message', `En ny användare har anslutit till spelet`);
+
+    // Run when user disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', `En användare har lämnat spelet`);
+    });
+})
 
 const PORT = 3000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
