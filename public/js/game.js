b40f499f06412853c8b6546a925ff7ba396477a3
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2022-09-06 15:07:04 +0200

    Added game board and did some css

---
+++ public/js/game.js
@@ -0,0 +1,47 @@
+var c_canvas = document.getElementById("c");
+var context = c_canvas.getContext("2d");
+
+//Drawing grid on canvas
+for (var x = 0.5; x < 525; x += 35) {
+  context.moveTo(x, 0);
+  context.lineTo(x, 525);
+}
+
+for (var y = 0.5; y < 525; y += 35) {
+  context.moveTo(0, y);
+  context.lineTo(525, y);
+}
+
+context.strokeStyle = "#ddd";
+context.stroke();
+
+// Set color user paints with
+const paintColor = "#00FF00"
+
+// Getting users mouse position
+ function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+      }
+
+// Getting the right square in the grid
+function getNearestSquare(position) {
+ var x = position.x;
+    var y = position.y;
+if (x < 0 || y < 0) return null;
+    x = (Math.floor(x / 35) * 35) + 0.5
+    y = (Math.floor(y / 35) * 35) + 0.5
+    return {x: x, y: y};
+}
+
+// Fill square with color
+$(c_canvas).click(function(evt) {
+    var pos = getNearestSquare(getMousePos(c_canvas, evt));
+    if (pos != null) {
+        context.fillStyle=paintColor;
+        context.fillRect(pos.x,pos.y,35,35);
+    }
+});
